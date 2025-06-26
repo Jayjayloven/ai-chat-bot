b7e771fa10902f6912e3da9c10a2547c47417197
A1C <jason_val.pilayan@us.af.mil>
2025-06-26 15:06:46 -0500

    working on chat layout

--- chat-bot/src/App.jsx
+++ chat-bot/src/App.jsx
@@ -1,10 +1,15 @@
 import { Chat } from "./components/chat/chat";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
+    <div className={styles.App}>
+      <section className={styles.Header}>
+        <img src="/chat-bot.png" alt="" className={styles.Logo}/>
+        <h4 className={styles.Title}>AI CHATBOT</h4>
+      </section>
       <Chat></Chat>
-    </>
+    </div>
   );
 }
 
--- chat-bot/src/components/chat/chat.jsx
+++ chat-bot/src/components/chat/chat.jsx
@@ -1,4 +1,30 @@
-export function Chat() {
-    
+import styles from "./chat.module.css";
+import { useState } from "react";
 
+export function Chat() {
+  const tempChats = [
+    { message: "Hey, how are you?" },
+    { message: "I'm good! How can I help you today?" },
+    { message: "Can you remind me to drink water?" },
+    { message: "Sure! I’ll remind you every 2 hours 😊" },
+    { message: "Thanks!" },
+    { message: "You're welcome!" },
+  ];
+  const [chats, setChats] = useState(tempChats);
+
+  return (
+    <div className={styles.Chat}>
+      <div className={styles.ChatContainer}>
+        {chats.map((chat, index)=>(<div key={index}>{chat}</div>))}
+      </div>
+      <div className={styles.InputContainer}>
+        <input
+          type="text"
+          placeholder="Message AI Chatbot"
+          className={styles.UserInput}
+        />
+        <img src="/send-icon.png" alt="" className={styles.MessageIcon} />
+      </div>
+    </div>
+  );
 }
